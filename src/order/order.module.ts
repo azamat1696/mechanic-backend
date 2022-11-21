4f28dc31f74f84d164a8a2ba0ad76060c4c63bfa
hal-efecan <hefecan@gmail.com>
2022-11-21 23:04:54 +0200

    removed orders controller

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,15 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { OrderController } from './controllers/order.controller';
 import { OrderService } from './services/order.service';
 import { Order } from './order.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order])],
-  controllers: [
-    /*OrderController*/
-  ],
+  controllers: [],
   providers: [OrderService],
   exports: [OrderService],
 })
