2b5beee04d3c04741ed82b80bd6d26af588a6761
hal-efecan <hefecan@gmail.com>
2023-01-05 18:58:11 +0200

    db configs

--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -7,14 +7,21 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
+        host: process.env.HOST,
         port: 3306,
-        username: 'halil',
-        password: 'H@lilh2o',
-        database: 'mechanic',
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
+
+      // type: 'mysql',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'halil',
+      // password: 'H@lilh2o',
+      // database: 'mechanic',
 
       return dataSource.initialize();
     },
