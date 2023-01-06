6dd497faaee5e90dc5afb8568d3d40c458a943c2
hal-efecan <hefecan@gmail.com>
2023-01-06 09:13:19 +0200

    db changes

--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -7,11 +7,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'eu-cdbr-west-03.cleardb.net',
+        host: process.env.HOST,
         port: 3306,
-        username: 'b2e12e90f2e7f2',
-        password: '730e5b61',
-        database: 'heroku_cc0f249cbafec76',
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
         ssl: true,
